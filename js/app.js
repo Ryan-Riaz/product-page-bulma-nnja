cd885b3987cfc2c116f3f6f39dfe628cde5f48b6
Ryan-Riaz <devryanriaz.gmail.com>
2021-01-19 20:59:40 +0600

    breadcrumb is added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log("Hello");
+// for activating the burger menu
+
+const bugerIcon = document.querySelector("#burger-icon");
+const navLinks = document.querySelector("#nav-links");
+
+bugerIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("is-active");
+});
